650bee03380a1170e8fbbcec3fa0e9475da93ca3
madhavpatel <madhav.patel@differenzsystem.com>
2025-02-18 11:55:11 +0530

    xAuth frontend commit on 18/Feb/2025

--- src/components/hooks/useTwitterAuth.js
+++ src/components/hooks/useTwitterAuth.js
@@ -40,7 +40,6 @@ export const useTwitterAuth = () => {
       const codeVerifier = generateCodeVerifier();
       const codeChallenge = await generateCodeChallenge(codeVerifier);
       // Store the code verifier in cookies
-      console.log("'check==>", cookieKeys, cookieKeys?.CODE_VERIFIER);
       Cookies.set(cookieKeys?.CODE_VERIFIER, codeVerifier);
       const params = new URLSearchParams({
         response_type: "code",
